16f3acdd8cf2ea941bcde47bceb82a550fa9631b
Mark <ymark@betcloud.com.au>
2022-04-10 13:10:23 +1000

    Test

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+
+export default function App() {
+  return (
+    <ChakraProvider>
+      <div>App</div>
+    </ChakraProvider>
+  );
+}
